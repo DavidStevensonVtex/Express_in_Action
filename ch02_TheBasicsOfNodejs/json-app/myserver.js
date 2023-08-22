e7ef86fbcd482a8409b1a8c8393aefdc67e3249e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 06:09:19 -0400

    Listing 2.14 Parsing the request URL with a request handler function

--- ch02_TheBasicsOfNodejs/json-app/myserver.js
+++ ch02_TheBasicsOfNodejs/json-app/myserver.js
@@ -1,16 +1,21 @@
-// Listing 2.13 A simple "Hello world" web server with Node
-
+// Listing 2.14 Parsing the request URL with a request handler function
 var http = require("http");
 
-function requestHandler(request, response) {
-    console.log("In comes a response to: " + request.url);
-    response.end("Hello, world!");
+function requestHandler(req, res) {
+    console.log("In comes a response to: " + req.url);
+       if (req.url === "/" ) {
+        res.end("Welcome to the homepage!");
+    } else if (req.url === "/about" ) {
+        res.end("Welcome to the about page!");
+    } else {
+        res.end("Error! File not found.");
+    }
 }
 
 var server = http.createServer(requestHandler);
 
 server.listen(3000) ;
 
-// In comes a response to: /abc
 // In comes a response to: /
-// In comes a response to: /goodbye
+// In comes a response to: /about
+// In comes a response to: /badpage
