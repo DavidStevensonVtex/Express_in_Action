dff33e66391ad885860aca3fe682fa07744cc3b9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 05:52:50 -0400

    Listing 2.11 Reading a file from disk

---
+++ ch02_TheBasicsOfNodejs/json-app/read-a-file-from-disk.js
@@ -0,0 +1,14 @@
+// Listing 2.11 Reading a file from disk
+
+var fs = require("fs");
+
+var options = { encoding: "utf-8" } ;
+fs.readFile("myfile.txt", options, function(err, data) {
+    if (err) {
+        console.error("Error reading file!") ;
+        return ;
+    }
+    console.log(data.match(/x/gi).length + " letter X's");
+});
+
+// 288 letter X's
