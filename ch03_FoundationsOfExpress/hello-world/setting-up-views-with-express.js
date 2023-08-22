2c4cef0936c36a3bc536a6b643608ad79d0406a9
David Stevenson <drstevenson1958@yahoo.com>
2023-08-22 11:07:50 -0400

    Listing 3.17 Rendering a view from Express

--- ch03_FoundationsOfExpress/hello-world/setting-up-views-with-express.js
+++ ch03_FoundationsOfExpress/hello-world/setting-up-views-with-express.js
@@ -1,9 +1,18 @@
-// Listing 3.15 Setting up views with Express
+// Listing 3.17 Rendering a view from Express
 
 var express = require("express");
 var path = require("path");
+var http = require("http");
 
 var app = express();
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.get("/", function(request, response) {
+    response.render("index", {
+        message: "Hey everyone! This is my webpage."
+    }) ;
+})
+
+http.createServer(app).listen(3000);
