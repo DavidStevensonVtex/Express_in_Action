5db5223a423fe614ea4ae97705671f938d431629
David Stevenson <drstevenson1958@yahoo.com>
2023-08-23 11:13:07 -0400

    Listing 4.8 app.js that now uses Morgan

--- ch04_Middleware/readme.md
+++ ch04_Middleware/readme.md
@@ -3,6 +3,8 @@
 ### 4.2 Example app: a static file server
 ### 4.2.5 Switching your logger to an open source one: Morgan
 
+MOrgan describes itself as "request logger middleware," which is exactly what you want.
+
 [Morgan logging software](https://github.com/expressjs/morgan)
 
 ```
--- ch04_Middleware/static-file-fun/app.js
+++ ch04_Middleware/static-file-fun/app.js
@@ -1,22 +1,18 @@
-// Listing 4.7 The first version of the static file app (app.js)
+// Listing 4.8 app.js that now uses Morgan
 
 var express = require("express") ;
+var morgan = require("morgan");
 var path = require("path") ;
 var fs = require("fs");
 
 var app = express();
 
-// Logs all incoming requests. Bug fixed with next(); statement.
-app.use( function(req, res, next) {
-    console.log("Request IP: " + req.url) ;
-    console.log("Request date: " + new Date()) ;
-    next();     // This is the critical new line.
-}) ;
+app.use(morgan("short"));
 
 app.use(function ( req, res, next ) {
     // Uses path.join to find the path where the file should be.
     var filePath = path.join(__dirname, "static", req.url);
-    console.log("__dirname", __dirname, "filePath", filePath, "req.url", req.url);
+    // console.log("__dirname", __dirname, "filePath", filePath, "req.url", req.url);
     fs.stat(filePath, function(err, fileInfo) {
         // If fs.stat fails, continue to the next middleware.
         if (err) {
