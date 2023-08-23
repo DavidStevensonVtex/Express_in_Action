c87f3b615b33ff40164c9b073b71891e6c0859ef
David Stevenson <drstevenson1958@yahoo.com>
2023-08-23 10:59:09 -0400

    Listing 4.5 Adding static file middleware to the middleware stack

--- ch04_Middleware/static-file-fun/app.js
+++ ch04_Middleware/static-file-fun/app.js
@@ -1,4 +1,4 @@
-// Listing 4.4 Fixing your logging middleware
+// Listing 4.5 Adding static file middleware to the middleware stack
 
 var express = require("express") ;
 var path = require("path") ;
@@ -12,6 +12,27 @@ app.use( function(req, res, next) {
     console.log("Request date: " + new Date()) ;
     next();     // This is the critical new line.
 }) ;
+
+app.use(function ( req, res, next ) {
+    // Uses path.join to find the path where the file should be.
+    var filePath = path.join(__dirname, "static", req.url);
+    console.log("__dirname", __dirname, "filePath", filePath, "req.url", req.url);
+    fs.stat(filePath, function(err, fileInfo) {
+        // If fs.stat fails, continue to the next middleware.
+        if (err) {
+            next();
+            return ;
+        }
+
+        if (fileInfo.isFile()) {
+            res.sendFile(filePath) ;
+        } else {
+            next();
+        }
+    });
+}) ;
+
+
 
 app.listen(3000, function() {
     console.log("App started on port 3000: http://localhost:3000/")
