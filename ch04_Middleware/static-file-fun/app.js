841873ffd0734d59453f36563d6281beaef20c86
David Stevenson <drstevenson1958@yahoo.com>
2023-08-23 10:35:52 -0400

    Listing 4.4 Fixing your logging middleware

--- ch04_Middleware/static-file-fun/app.js
+++ ch04_Middleware/static-file-fun/app.js
@@ -1,4 +1,4 @@
-// Listing 4.3 Start app.js for your static file server
+// Listing 4.4 Fixing your logging middleware
 
 var express = require("express") ;
 var path = require("path") ;
@@ -6,10 +6,11 @@ var fs = require("fs");
 
 var app = express();
 
-// Logs all incoming requests. (This has a bug. Forget next(); line ?)
+// Logs all incoming requests. Bug fixed with next(); statement.
 app.use( function(req, res, next) {
     console.log("Request IP: " + req.url) ;
     console.log("Request date: " + new Date()) ;
+    next();     // This is the critical new line.
 }) ;
 
 app.listen(3000, function() {
