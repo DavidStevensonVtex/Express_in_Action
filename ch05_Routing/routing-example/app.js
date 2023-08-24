ac78ef0e40605456a39c1e153fd2dae8f921d72e
David Stevenson <drstevenson1958@yahoo.com>
2023-08-24 10:41:15 -0400

    Listing 5-2. Simplifying response code for simplest parameter

--- ch05_Routing/routing-example/app.js
+++ ch05_Routing/routing-example/app.js
@@ -8,16 +8,10 @@ app.get("/olivia", function(request, response) {
 });
 
 app.get("/users/:userid", function(req, res) {
-    try {
-        let userId = parseInt(req.params.userid, 10);
-        if (Number.isNaN(userId)) {
-            res.send("Invalid user: user id must be a number.");
-        } else {
-            res.send("Welcome user: " + userId);
-        }
-    } catch (e) {
-        console.error("User ids must be a valid integer: ", req.params.userid, "\n", e) ;
-    }
+    let userId = parseInt(req.params.userid, 10);
+    res.send( Number.isNaN(userId) ? 
+        "Invalid user: user id must be a number." :
+        "Welcome user: " + userId);
 }) ;
 
 app.use( function(request, response) {
