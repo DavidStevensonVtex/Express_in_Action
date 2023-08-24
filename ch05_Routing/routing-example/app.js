25e7c85b3017737e9684976844732a2c9d6f5403
David Stevenson <drstevenson1958@yahoo.com>
2023-08-24 10:38:36 -0400

    Listing 5-2. The simplest parameter

--- ch05_Routing/routing-example/app.js
+++ ch05_Routing/routing-example/app.js
@@ -1,11 +1,24 @@
-// Listing 5-1. A simple Express app that shows Olivia's homepage
+// Listing 5-2. The simplest parameter
 
-var express = require("express");
-var app = express();
+let express = require("express");
+let app = express();
 
 app.get("/olivia", function(request, response) {
     response.send("Welcome to Olivia's homepage!");
 });
+
+app.get("/users/:userid", function(req, res) {
+    try {
+        let userId = parseInt(req.params.userid, 10);
+        if (Number.isNaN(userId)) {
+            res.send("Invalid user: user id must be a number.");
+        } else {
+            res.send("Welcome user: " + userId);
+        }
+    } catch (e) {
+        console.error("User ids must be a valid integer: ", req.params.userid, "\n", e) ;
+    }
+}) ;
 
 app.use( function(request, response) {
     response.status(404).send("Page not found!");
