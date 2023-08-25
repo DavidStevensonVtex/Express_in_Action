ce7fb7bf1b29ea3c646a1a4ca486fe6ccdb696db
David Stevenson <drstevenson1958@yahoo.com>
2023-08-25 09:28:16 -0400

    Listing 6.7 Version 2 of your API, in api2.js

---
+++ ch06_BuildingAPIs/random-number-api/api2.js
@@ -0,0 +1,11 @@
+// Listing 6.7 Version 2 of your API, in api2.js
+
+let express = require("express");
+
+let api = express.Router() ;
+
+api.get("/timezone", function ( req, res) {
+    res.send("API 2: super cooll new response for /timezone");
+});
+
+module.exports = api ;      // Exports the router so that other files can use it
