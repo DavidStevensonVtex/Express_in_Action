cc15b6e905101821ceab9e5c21f0fee879679849
David Stevenson <drstevenson1958@yahoo.com>
2023-08-25 09:30:52 -0400

    Listing 6.8 The main app code in app.js

--- ch06_BuildingAPIs/random-number-api/app.js
+++ ch06_BuildingAPIs/random-number-api/app.js
@@ -1,12 +1,14 @@
-// Listing 6.6 The main app code in app.js
+// Listing 6.8 The main app code in app.js
 
 let express = require("express");
 
 let apiVersion1 = require("./api1.js");
+let apiVersion2 = require("./api2.js");
 
 let app = express();
 
 app.use("/v1", apiVersion1);
+app.use("/v2", apiVersion2);
 
 app.listen(3000, function() {
     console.log("App started on port 3000: http://localhost:3000/")
