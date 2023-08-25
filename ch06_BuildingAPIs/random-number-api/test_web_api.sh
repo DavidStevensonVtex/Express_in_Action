65a02f0327a1d6425c241ff9fdf1b3f770df2ea0
David Stevenson <drstevenson1958@yahoo.com>
2023-08-25 09:07:24 -0400

    Testing the Web API with curl Linux commands

---
+++ ch06_BuildingAPIs/random-number-api/test_web_api.sh
@@ -0,0 +1,4 @@
+curl -w "\n" -X GET    http://localhost:3000
+curl -w "\n" -X POST   http://localhost:3000
+curl -w "\n" -X PUT    http://localhost:3000
+curl -w "\n" -X DELETE http://localhost:3000
