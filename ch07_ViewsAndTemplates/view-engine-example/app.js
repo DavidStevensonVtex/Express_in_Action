340e7272f4950b5f9a757e0cd689c0667375a069
David Stevenson <drstevenson1958@yahoo.com>
2023-08-26 10:45:44 -0400

    Adding the index.ejs view

--- ch07_ViewsAndTemplates/view-engine-example/app.js
+++ ch07_ViewsAndTemplates/view-engine-example/app.js
@@ -8,6 +8,10 @@ let app = express();
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 
+app.get("/", function(req, res) {
+    res.render("index") ;
+}) ;
+
 app.listen(3000, function() {
     console.log("App started on port 3000: http://localhost:3000/")
 });
