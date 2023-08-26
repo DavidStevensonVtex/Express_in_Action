d1ab9db9860837655f34b35476ebd60a4fe657dd
David Stevenson <drstevenson1958@yahoo.com>
2023-08-26 10:36:58 -0400

    Listing 7.1 Simple view rendering example

---
+++ ch07_ViewsAndTemplates/view-engine-example/app.js
@@ -0,0 +1,13 @@
+// Listing 7.1 Simple view rendering example
+
+let express = require("express");
+let path = require("path");
+
+let app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+
+app.listen(3000, function() {
+    console.log("App started on port 3000: http://localhost:3000/")
+});
