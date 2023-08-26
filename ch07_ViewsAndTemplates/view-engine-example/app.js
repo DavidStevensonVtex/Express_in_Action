61e1939a32cb3ced8d78af224c4ee9a0a22ae45c
David Stevenson <drstevenson1958@yahoo.com>
2023-08-26 11:25:25 -0400

    Create some EJS views for testing

--- ch07_ViewsAndTemplates/view-engine-example/app.js
+++ ch07_ViewsAndTemplates/view-engine-example/app.js
@@ -1,15 +1,41 @@
-// Listing 7.1 Simple view rendering example
+// Listing 7.2 Complex rendering example
 
 let express = require("express");
 let path = require("path");
+let ejs = require("ejs");
 
 let app = express();
 
+app.locals.appName = "Song Lyrics" ;
+
 app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
+app.engine("html", ejs.renderFile);
+
+app.use(function(req, res, next) {
+    res.locals.userAgent = req.headers["user-agent"] ;
+    next() ;
+});
+
+app.get("/about", function(req, res) {
+    res.render("about", {
+        currentUser: "india-arie123"
+    }) ;
+}) ;
+
+app.get("/contact", function(req, res) {
+    res.render("contact.ejs");
+});
 
 app.get("/", function(req, res) {
     res.render("index") ;
+}) ;
+
+app.use(function(req, res) {
+    res.status(404);
+    res.render("404.ejs", { 
+        urlAttempted: req.url
+    }) ;
 }) ;
 
 app.listen(3000, function() {
